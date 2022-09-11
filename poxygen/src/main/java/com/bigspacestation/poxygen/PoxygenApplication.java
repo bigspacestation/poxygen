8c52186e86d369de3961757642dc28882c541f2f
BigSpaceStation <bigspacestation@gmail.com>
2022-09-11 18:28:20 +0300

    Spring boot project has been created.

---
+++ poxygen/src/main/java/com/bigspacestation/poxygen/PoxygenApplication.java
@@ -0,0 +1,13 @@
+package com.bigspacestation.poxygen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoxygenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoxygenApplication.class, args);
+	}
+
+}
---
+++ poxygen/src/test/java/com/bigspacestation/poxygen/PoxygenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigspacestation.poxygen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoxygenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
